cda8c993e93d0518d79163c3ae307fcc41fa7d6e
Pierre Kiessling <apfel@abwesend.de>
2018-10-26 10:17:56 +0200

    versuch mit setstate

--- src/Board.js
+++ src/Board.js
@@ -2,55 +2,36 @@ import React, {Component} from 'react';
 import ReactTable from "react-table";
 import './Board.css';
 import 'react-table/react-table.css';
+import axios from "axios";
 
 export default class Board extends Component {
 
+    state = {
+        tasks: []
+    };
 
-    constructor(){
-        super();
-        this.data = [];
+    componentDidMount() {
+        axios
+            .get('http://h2806881.stratoserver.net:8080/api/tasks')
+            .then(response => {
 
-    }
+                const newTasks = response.map(task => {
+                    return {
+                        id: task.id,
+                        name: task.name
+                    }
+                });
 
+                const newState = Object.assign({}, this.state, {
+                    tasks: newTasks
+                });
 
-    getData(){
-        fetch('http://h2806881.stratoserver.net:8080/api/tasks', {'mode': 'no-cors'})
-            .then(result => {
-                console.log(result);
-                return result.json();
+                this.setState(newState);
             })
-            .then(result  =>
-                    this.data = result
-            )
-            .catch(error => console.log(error))
-
-
+            .catch(error => console.log(error));
     }
+
     render() {
-        this.getData();
-
-        const data2 = [{
-            id: '123',
-            name: 'Task1',
-            state: 'wip',
-            creatorId: '',
-            ownerId: ''
-        },
-            {
-                id: '123',
-                name: 'Task2',
-                state: 'todo',
-                creatorId: '',
-                ownerId: ''
-            },
-            {
-                id: '123',
-                name: 'Task3',
-                state: 'done',
-                creatorId: '',
-                ownerId: ''
-            }];
-
         const columnsWiP = [{
             Header: 'WiP',
             accessor: 'name' // String-based value accessors!
@@ -66,11 +47,14 @@ export default class Board extends Component {
             accessor: 'name' // String-based value accessors!
         }];
 
-        this.data = this.data.filter(item => item !== null);
-        const stateToDo = this.data.filter(item => item.state === 'todo');
-        const stateWiP = this.data.filter(item => item.state === 'wip');
-        const stateDone = this.data.filter(item => item.state === 'done');
+        let stateToDo;
+        let stateWiP;
+        let stateDone;
 
+        this.state.tasks = this.state.tasks.filter(item => item !== null);
+        stateToDo =  this.state.tasks.filter(item => item.state === 'open');
+        stateWiP =  this.state.tasks.filter(item => item.state === 'wip');
+        stateDone =  this.state.tasks.filter(item => item.state === 'done');
 
         return (
             <div>
@@ -83,7 +67,6 @@ export default class Board extends Component {
                 <div className="col">
                     <ReactTable data={stateDone} columns={columnsDone}/>
                 </div>
-
             </div>
         )
     }
